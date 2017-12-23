d1eba28e98aa2c2c2f8e8a50d8f2c4a20939b35c
Chas Meyer <chas.meyer@gmail.com>
2017-12-23 11:21:13 -0500

    asdf

---
+++ elpa/ht-20171213.1334/ht-autoloads.el
@@ -0,0 +1,15 @@
+;;; ht-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ht.el") (23102 24723 600637 260000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ht-autoloads.el ends here
